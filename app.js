fdd97e2b2fd13bde2dc865284820782cc32dafb2
Anurag Choudhury <anuragch198@gmail.com>
2023-02-15 11:26:40 +0530

    fixed bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import qrcode from 'qrcode';
 import path from 'path';
 const app = express();
-const port = 3000||process.env.PORT;
+const port = process.env.PORT;
 
 app.use(express.json());
 
